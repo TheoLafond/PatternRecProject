8e41c05270e625b5b0c9e0357a9ffcb2cb4f8196
TheoLafond <118629057+TheoLafond@users.noreply.github.com>
2023-05-10 00:45:08 +0200

    add frequency treatment

--- extracters.py
+++ extracters.py
@@ -22,6 +22,13 @@ def vote_th_filter(vect,th,n,q):
             out[i] = 0
     return out
 
+def replace_by_previous(vect,th):
+    out = vect.copy()
+    for i in range(1,vect.size):
+        if (np.abs(vect[i-1]-vect[i])<th):
+            out[i] = out[i-1]
+    return out
+
 def quantile_threshold(vect):
     threshold = np.quantile(vect, 0.1)
     return vect>threshold
@@ -50,6 +57,22 @@ def expend_low(vect,th,size_brush):
         
     return out
 
+def compute_notes(vect,note):
+    list_notes = []
+    pitch = []
+    i=0
+    while (i<vect.size):
+        if (not note[i]):
+            if(len(pitch)!=0):
+                list_notes.append(sum(pitch)/len(pitch))
+                pitch = []
+            while((i<vect.size) and (not note[i]) ):
+                i+=1
+            if (i>=vect.size):
+                break
+        pitch.append(vect[i])
+        i+=1
+    return list_notes
 
 def transform_co(vect):
     vect = low_pass_filter(vect, 10)
@@ -58,12 +81,23 @@ def transform_co(vect):
     return vect
 
 def transform_intensity(vect):
-    vect = low_pass_filter(vect, 4)
-    vect = grad(vect)
-    vect = expend_high(vect,270,5)
-    vect = expend_low(vect,270,5)
-    vect = (vect - 300)/70
-    vect = 1/(1+np.exp(-vect))
+    th = threshold_otsu(vect)
+    return vect>th
+
+def transform_fe(vect,note):
+    vect = np.log(vect)
+    pause = (vect>6.5)+np.logical_not(note)
+    note = np.logical_not(pause)
+    vect[np.where(pause)]=-1
+    min_note = min(compute_notes(vect, note))
+    if(min_note>np.log(32)):
+        while(min_note>np.log(65)):
+            vect[note] -= np.log(2)
+            min_note -= np.log(2)
+    else : 
+        while(min_note<np.log(32)):
+            vect[note] += np.log(2)
+            min_note += np.log(2)
     return vect
 
 def main_extractor(features):
@@ -71,8 +105,10 @@ def main_extractor(features):
     co = features[1,:]
     intensity = features[2,:]
     
-    co_pause_prob = transform_co(co)
-    
+    #co_pause_prob = transform_co(co)
+    inote = transform_intensity(intensity)
+    processed_fe = transform_fe(fe, inote)
+    return processed_fe    
 
 
 
